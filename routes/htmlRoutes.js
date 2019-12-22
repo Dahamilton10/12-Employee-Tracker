06699cdb8191ae8b58b6f381364cf86848fabeb3
David Hamilton <dahamilton10@yahoo.com>
2019-12-22 14:32:24 -0500

    got to the point where I can pull data from my db. Still not rendering to the page though

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,20 @@
 const path = require("path");
 const router = require("express").Router();
+const connection = require("../db/dbConnection");
 
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  connection.query("SELECT * FROM employee;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+    console.log(data);
+    res.render("index", { plans: data });
+  });
 });
 
 // All other routes respond with the index.html file
-router.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 module.exports = router;
